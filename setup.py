749ca7865bc7aa55cb48d82c95ff8d52b99f1a31
Gary Wilson Jr <gary@thegarywilson.com>
2012-07-28 01:37:43 -0500

    Added classifiers.

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import setup, find_packages
 
 
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Topic :: Communications',
+    'Topic :: Scientific/Engineering',
+    'Topic :: System :: Clustering',
+]
+
+
 setup(
     name='paxos',
     version='0.1',
+    classifiers=CLASSIFIERS,
     author='Gary Wilson Jr. and Michael Stathopoulos',
 #    author_email='',
     packages=find_packages(),
